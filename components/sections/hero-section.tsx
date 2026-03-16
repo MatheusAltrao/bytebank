7d1e53ecbc67712f2b3758417a44cb58843894ca
Matheus Altrao <matheusaltrao2@gmail.com>
2026-03-16 07:35:09 -0400

    fix: corrigir classes de espaçamento no componente HeroSection

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -2,7 +2,7 @@ import AvaliableAmountCard from './components/avaliable-amount-card'
 
 export default function HeroSection() {
   return (
-    <div className="bg-secondary p-8 rounded-xl flex flex-col gap-4">
+    <div className="bg-secondary py-8 px-2 rounded-xl flex flex-col gap-4">
       <div>
         <h3 className="text-2xl font-medium">Olá, Seja bem vindo(a)</h3>
         <p className="text-muted-foreground">Estamos muito felizes em te ver por aqui!</p>
